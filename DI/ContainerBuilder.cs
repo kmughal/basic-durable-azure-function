d6839131797ba5af28864c23f2b55c3b900619a3
Khurram Shahzad <KhurramShahzad@tfl.gov.uk>
2018-12-26 23:50:19 +0000

    Add DI to make unit test easy

---
+++ DI/ContainerBuilder.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DurableFunctionsSample.DI
+{
+
+    public interface ICalculator
+    {
+        int Add(int input1, int input2);
+    }
+
+    public interface IDoubler
+    {
+        int Double(int value);
+    }
+
+    public class Doubler  : IDoubler
+    {
+        public int Double(int value)
+        {
+            return value * value;
+        }
+    }
+
+    public class Calculator : ICalculator
+    {
+        private readonly IDoubler _doubler;
+
+        public Calculator(IDoubler doubler)
+        {
+            _doubler = doubler;
+        }
+
+        public int Add(int input1, int input2)
+        {
+            return _doubler.Double(input1) + _doubler.Double(input2);
+        }
+    }
+
+    public interface IContainerBuilder
+    {
+        ServiceProvider Build();
+    }
+    
+    public class ContainerBuilder : IContainerBuilder
+    {
+        private readonly IServiceCollection serviceCollection;
+        public ContainerBuilder()
+        {
+            serviceCollection = new ServiceCollection();
+            RegisterServices();
+        }
+
+        private void RegisterServices()
+        {
+            serviceCollection.AddScoped(typeof(ICalculator), typeof(Calculator));
+            serviceCollection.AddScoped(typeof(IDoubler), typeof(Doubler));
+        }
+
+
+        public ServiceProvider Build()
+        {
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
--- Function1.cs
+++ Function1.cs
@@ -8,18 +8,25 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Net.Http;
+using DurableFunctionsSample.DI;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DurableFunctionsSample
 {
     //  "AzureWebJobsSecretStorageType": "files" add this in the local.settings.json // there is a bug -> Emulator.
     public static class Function1
     {
+        private static ServiceProvider services = new ContainerBuilder().Build();
+
         [FunctionName("DurableFunctionSample")]
         public static async Task<HttpResponseMessage> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestMessage req,
             [OrchestrationClient] DurableOrchestrationClient client,
             ILogger log)
         {
+            var calculator = services.GetService<ICalculator>();
+            var result = calculator.Add(2, 5);
+
             string process = req.RequestUri.ParseQueryString().Get("process");
             if (process == null) return req.CreateResponse(System.Net.HttpStatusCode.BadRequest);
             var id = await client.StartNewAsync("O_MainActivity", process);
